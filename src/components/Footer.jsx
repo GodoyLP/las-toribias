0016edb018ae6dd68bb83b3c468298f4226c81b3
LuisPG <luispedrogodoy@gmail.com>
2026-01-03 22:07:11 -0300

    arreglo footer por vercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-/*import '../styles/stylefooter.css'; // Importamos los estilos del footer*/
+
 
 const Footer = () => {
   return (
